99020a5323c62c45029e19d791ffa097a7f83a45
BruninSouza <brun.souz4@gmail.com>
2025-07-26 13:02:45 -0300

    "refactor: Implementa verificação se a coluna "Serviço" existe no XLSX usado em  01_Criar_Coluna_Servico.py"

--- Ajuste-Colunas-Numericas/codigos/01_Criar_Coluna_Servico.py
+++ Ajuste-Colunas-Numericas/codigos/01_Criar_Coluna_Servico.py
@@ -13,8 +13,12 @@ mapa_para_numerico = {sigla: numero for numero, sigla in MAPA_NOME_PARA_SIGLA.it
 try:
     print("\n=== Iniciando processo de gerar dataset com valores númericos... ===")
     df_a = pd.read_excel(caminho_db_a)
-    df_a["Serviço"] = df_a["Serviço"].map(mapa_para_numerico)
-    df_a.to_excel(caminho_arquivo_saida, index=False)
-    print("✅Coluna Serviço Atualizada com Sucesso!!")
+    if "Serviço" in df_a.columns:
+        df_a["Serviço"] = df_a["Serviço"].map(mapa_para_numerico)
+        df_a["Serviço"] = df_a["Serviço"].fillna(99)
+        df_a.to_excel(caminho_arquivo_saida, index=False)
+        print("✅Coluna Serviço Atualizada com Sucesso!!")
+    else:
+        print(f"ℹ️ Coluna 'Serviço' não encontrada no arquivo '{os.path.basename(caminho_db_a)}'. Nenhuma alteração de serviço feita.")        
 except FileNotFoundError as e:
     print(f"Erro: Arquivo não encontrado. Verifique se os nomes dos arquivos estão corretos. Detalhes: {e}")
